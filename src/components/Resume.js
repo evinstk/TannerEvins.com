86f831cb7651717c64f06de8379b0d53219a5ca1
Tanner Evins <tanner@tannerevins.com>
2017-04-10 13:12:44 -0500

    Generate languages and skill sections with map

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,23 +1,46 @@
 import React from 'react'
 
+const languages = [
+    'JavaScript (ECMAScript 6)',
+    'C',
+    'C++11/C++14',
+    'C#',
+    'Python',
+    'Haskell',
+    'Scala',
+    'Clojure',
+    'SQL'
+]
+
+const software = [
+    'Node.js',
+    'npm',
+    'React',
+    'Redux',
+    'Flux',
+    'Lodash',
+    'Gulp',
+    'Browserify',
+    'Babel',
+    'AngularJS',
+    'Git',
+    'Emacs',
+    'Vim',
+    'Bash shell',
+    'GNU/Linux utilities',
+    'Visual Studio'
+]
+
+const mapLi = arr => arr.map((item, index) => <li key={index}>{item}</li>)
+
 const Resume = () => (
   <div>
     <div>Tanner Evins</div>
     <div>
-      <div>Skills and Technologies</div>
-      <ul>
-        <li>
-          <span>JavaScript (including ES6)</span>
-          <ul>
-            <li>Node.js</li>
-            <li>npm</li>
-            <li>React</li>
-            <li>Redux</li>
-          </ul>
-        </li>
-        <li>C++11/C++14</li>
-        <li>C#</li>
-      </ul>
+      <div className="section">languages</div>
+      <ul>{mapLi(languages)}</ul>
+      <div className="section">software</div>
+      <ul>{mapLi(software)}</ul>
     </div>
   </div>
 )

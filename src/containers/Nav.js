90c884af7cc0773297e1643ab7d78e7f3342df12
Tanner Evins <tanner@tannerevins.com>
2017-04-11 22:18:56 -0500

    Style navigation

--- src/containers/Nav.js
+++ src/containers/Nav.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router'
 const Nav = () => (
   <nav className="site-nav">
     <ul role="nav">
-      <li><Link to='/resume'>Resume</Link></li>
+      <li><Link to="/resume" activeClassName="active">Resume</Link></li>
     </ul>
   </nav>
 )
